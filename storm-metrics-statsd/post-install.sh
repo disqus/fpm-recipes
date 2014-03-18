04da18d3f4d186551d319b393fafc549618e24cf
John Watson <john@disqus.com>
2014-03-18 19:39:32 +0000

    Add storm-metrics-statsd recipe

---
+++ storm-metrics-statsd/post-install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd /usr/share/java
+ln -s storm-metrics-statsd-1.0.0-SNAPSHOT.jar storm-metrics-statsd.jar
+ln -s java-statsd-client-2.0.0.jar java-statsd-client.jar
---
+++ storm-metrics-statsd/pre-uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm /usr/share/java/storm-metrics-statsd.jar
+rm /usr/share/java/java-statsd-client.jar
